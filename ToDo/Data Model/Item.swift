4cd3c64a8d32fdec8986a0a1ab127706fbad7542
Brandon Landgraff <brandon.landgraff@gmail.com>
2019-07-08 20:47:52 -0500

    Convert to MVC design pattern.

---
+++ ToDo/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by Brandon Landgraff on 2019-07-07.
+//  Copyright © 2019 Brandon Landgraff. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+    
+}
